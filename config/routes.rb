cadae9b52e719749d7ea9ce931c94ef15a8503d1
gokul2210krishnan <gokul2210krishnan@gmail.com>
2024-04-28 23:06:29 +0530

    Adding index as default for 'rails server' command
    Signed-off-by: gokul2210krishnan <gokul2210krishnan@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "sorters#index"
+  root "sorters#index"
   get "sorters", to: "sorters#index"
   # root "articles#index"
 end
